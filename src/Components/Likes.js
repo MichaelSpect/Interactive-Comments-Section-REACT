8b6c0236a76f9eefd3bace50e26d1beb87a7403e
Michael Spect (Miodrag Mladenovic) <michaels.spect@gmail.com>
2022-01-13 13:00:55 +0100

    Change - Fix Likes fn - state setAllComments - working

--- src/Components/Likes.js
+++ src/Components/Likes.js
@@ -1,32 +1,34 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./Styles/Likes.css";
 
 const Likes = ({ score, allComments, setAllComments, id }) => {
-  const [commScore, setCommScore] = useState(score);
+  // const [commScore, setCommScore] = useState(score);
 
-  const clickScorePlusHandler = function () {
-    setCommScore((prevScore) => prevScore + 1);
-    changeScoreStateHandler();
-  };
-  const clickScoreMinusHandler = function () {
-    setCommScore((prevScore) => prevScore - 1);
-    changeScoreStateHandler();
-  };
-  const changeScoreStateHandler = function () {
-    const newArr = allComments.map(function (comment) {
-      console.log(comment.score, commScore);
-      if (comment.id === id) return { ...comment, score: commScore };
-      const filteredReplies = comment.replies.map((comment) =>
-        comment.id === id ? { ...comment, score: commScore } : comment
-      );
-      return { ...comment, replies: filteredReplies };
+  const changeScoreStateHandler = function (newScore) {
+    // setCommScore((prevScore) => newScore);
+    setAllComments((prevComents) => {
+      const newCommentsArr = prevComents.map(function (comment) {
+        const newCalculatedScore = newScore;
+        // Setting new score if it's first-line (parent) comment
+        if (comment.id === id) return { ...comment, score: newCalculatedScore };
+        // Setting new score if it's reply (children) comment
+        const filteredReplies = comment.replies.map((comment) =>
+          comment.id === id
+            ? { ...comment, score: newCalculatedScore }
+            : comment
+        );
+        return { ...comment, replies: filteredReplies };
+      });
+      return newCommentsArr;
     });
-    setAllComments((prevComents) => newArr);
   };
 
   return (
     <div className="likes-container">
-      <div className="score-plus" onClick={clickScorePlusHandler}>
+      <div
+        className="score-plus"
+        onClick={() => changeScoreStateHandler(score + 1)}
+      >
         <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
@@ -35,8 +37,11 @@ const Likes = ({ score, allComments, setAllComments, id }) => {
         </svg>
       </div>
 
-      <div className="score-value">{commScore}</div>
-      <div className="score-minus" onClick={clickScoreMinusHandler}>
+      <div className="score-value">{score}</div>
+      <div
+        className="score-minus"
+        onClick={() => changeScoreStateHandler(score - 1)}
+      >
         <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
